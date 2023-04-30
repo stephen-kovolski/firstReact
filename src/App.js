6bbfbabf9d643eaa30bb4268ee2b3eb923501694
Stephen Kovolski <s.kovolski@gmail.com>
2023-04-29 23:29:40 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import {useEffect} from 'react';
+
+const API_URL = 'http://www.omdbapi.com?apikey=37052d82'
+
+
+const App = () => {
+    
+    const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    console.log(data.Search);
+    }
+
+
+    useEffect(() => {
+            searchMovies('Batman')
+    }, []);
+
+
+    return (
+        <h1>App</h1>
+    );
+}
+
+export default App;
