6b5754c500dba3b812288e3d23c11968029710c9
Stephen Kovolski <s.kovolski@gmail.com>
2023-04-30 21:57:11 -0400

    finished react movie site

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
+import './App.css';
+import SearchIcon from './search.svg'
+import MovieCard from './movieCard';
 
 const API_URL = 'http://www.omdbapi.com?apikey=37052d82'
-
+const movie1 = {
+    Title: "Batman",
+    Type: "movie",
+    Year: "1989",
+    imdbID: "tt0096895",
+    Poster: "N/A"
+}
 
 const App = () => {
-    
+    const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
     const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
     const data = await response.json()
 
-    console.log(data.Search);
+    setMovies(data.Search);
     }
 
 
@@ -19,7 +29,39 @@ const App = () => {
 
 
     return (
-        <h1>App</h1>
+        <div className='app'>
+            <h1>MovieLand</h1>
+            <div className='search'>
+                <input
+                placeholder='Search for Movies'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img
+                src={SearchIcon}
+                alt='search'
+                onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0
+                  ? (
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                  ) : (
+                    <div className='empty'>
+                        <h2>No Movies Found</h2>
+                    </div>
+                  )}
+
+            
+        </div>
+
+
+
     );
 }
 
--- src/movieCard.jsx
+++ src/movieCard.jsx
@@ -1,23 +1,24 @@
 
 import React from 'react';
 
-const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
-  return (
-    <div className="movie" key={imdbID}>
-      <div>
-        <p>{Year}</p>
-      </div>
-
-      <div>
-        <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
-      </div>
-
-      <div>
-        <span>{Type}</span>
-        <h3>{Title}</h3>
-      </div>
+const MovieCard = ({movie}) => {
+    return(
+<div className='movie'>
+    <div>
+        <p>{movie.Year}</p>
     </div>
-  );
+    <div>
+        <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
+    </div>
+    <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+    </div>
+</div>
+    )
 }
+
+
+
 
 export default MovieCard;
